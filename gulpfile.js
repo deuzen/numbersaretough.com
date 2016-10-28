b0ddb1911ee6227ea49852a9a9f5477716d21b4c
deuzen <davidxtejada@gmail.com>
2016-10-28 16:41:13 +0200

    Initial setup

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const gulp = require('gulp');
+const semanticBuild = require('./app/vendor/semantic/tasks/build');
+const semanticClean = require('./app/vendor/semantic/tasks/clean');
+const browserSync = require('browser-sync').create();
+const runSequence = require('run-sequence');
+const del = require('del');
+
+/*--------------
+    Semantic
+---------------*/
+
+gulp.task('semantic:build', semanticBuild);
+gulp.task('semantic:cleanDist', semanticClean);
+gulp.task('semantic:cleanBuild', () => {
+  return del('build/assets/semantic');
+});
+
+gulp.task('semantic', (callback) => {
+  runSequence(['semantic:cleanDist', 'semantic:cleanBuild'], 'semantic:build', callback);
+});
+
+/*--------------
+      HTML
+---------------*/
+
+gulp.task('html', () => {
+  gulp.src('app/index.html')
+    .pipe(gulp.dest('build/'))
+});
+
+/*--------------
+  browser-sync
+---------------*/
+
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    }
+  });
+});
+
+gulp.task('reload', () => {
+  browserSync.reload();
+});
+
+/*--------------
+     default
+---------------*/
+
+gulp.task('default', (callback) => {
+  runSequence('html', 'browser-sync');
+
+  gulp.watch('app/index.html', () => {
+    runSequence('html', 'reload', callback);
+  });
+});
